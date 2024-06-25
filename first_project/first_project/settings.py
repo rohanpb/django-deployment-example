ec70fcd115fe7447bb34d60ba0cfb856d8e9d7a7
rohanpb <rohanparab1997@gmail.com>
2024-06-25 16:08:08 +0530

    added host

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-i&p@v@dj=@btq@st^67hu19=z(o(lehjaik&6#ir)dqgzlny9i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
